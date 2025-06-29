e1d6e2454db4ded14606b622cbecf43ee6f00df1
Jae <comea22@gmail.com>
2025-06-29 20:10:33 +0800

    feat: update to custom domain and re-enable CSRF protection

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -136,5 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-SHORTENER_BASE_URL = 'https://url-shortener-production-dfb0.up.railway.app/'
+SHORTENER_BASE_URL = 'https://brief.baby/'
 TAILWIND_APP_NAME = 'theme'
