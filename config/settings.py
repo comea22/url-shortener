5313fe18b6d9c95a5c042f7bd88d613f4b0f768c
Jae <comea22@gmail.com>
2025-06-29 22:24:46 +0800

    update: change base URL in settings

--- config/settings.py
+++ config/settings.py
@@ -136,5 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-SHORTENER_BASE_URL = 'https://brief.baby/'
+SHORTENER_BASE_URL = 'https://www.brief.baby/'
 TAILWIND_APP_NAME = 'theme'
